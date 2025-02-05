ae774dbfd3c820738c5c58a6c62b7f7699667a73
Ibon <www.ibonhoz@gmail.com>
2025-02-05 13:39:01 +0100

    Primero

---
+++ examen_usuarios.ps1
@@ -0,0 +1,2 @@
+﻿New-LocalUser -Name "usu1examen" -Password (ConvertTo-SecureString "Contraseña123" -AsPlainText -Force) -FullName "Usuario 1 Examen" -Description "Usuario creado para examen"
+New-LocalUser -Name "usu2examen" -Password (ConvertTo-SecureString "Contraseña123" -AsPlainText -Force) -FullName "Usuario 2 Examen" -Description "Usuario creado para examen"
